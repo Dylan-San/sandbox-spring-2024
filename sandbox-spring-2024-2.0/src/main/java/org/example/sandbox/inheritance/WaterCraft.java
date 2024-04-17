225673bbc1fa410e98e68745d40e80a7ed741385
Dylan-San <156867354+Dylan-San@users.noreply.github.com>
2024-04-16 19:47:10 -0700

    Add files via upload

---
+++ sandbox-spring-2024-2.0/src/main/java/org/example/sandbox/inheritance/WaterCraft.java
@@ -0,0 +1,11 @@
+package org.example.sandbox.inheritance;
+
+import org.example.sandbox.inheritance.Vehicle;
+
+public class WaterCraft extends Vehicle {
+
+    @Override
+    public void transport() {
+
+    }
+}
